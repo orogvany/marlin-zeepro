6add4739ba488f236b802a505052389255c637da
chris.orogvany <chris.orogvany@theplatform.com>
2016-11-30 15:52:27 -0800

    minor formatting

--- Configuration.h
+++ Configuration.h
@@ -1,15 +1,14 @@
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
 
-// This configurtion file contains the basic settings.
+// This configuration file contains the basic settings.
 // Advanced settings can be found in Configuration_adv.h 
-// BASIC SETTINGS: select your board type, temperature sensor type, axis scaling, and endstop configuration
 
 //User specified version info of this build to display in [Pronterface, etc] terminal window during startup.
 //Implementation of an idea by Prof Braino to inform user that any changes made
 //to this build by the user have been successfully uploaded into firmware.
 #define STRING_VERSION_CONFIG_H __DATE__ " " __TIME__ // build date and time
-#define STRING_CONFIG_H_AUTHOR "(JPod, Zim_Board_v6)" //Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(waffles, Zim_Board_v6)" //Who made the changes.
 
 // SERIAL_PORT selects which serial port should be used for communication with the host.
 // This allows the connection of wireless adapters (for instance) to non-default port pins.
@@ -19,71 +18,19 @@
 // This determines the communication speed of the printer
 #define BAUDRATE 250000
 
-
-//// The following define selects which electronics board you have. Please choose the one that matches your setup
-// 10 = Gen7 custom (Alfons3 Version) "https://github.com/Alfons3/Generation_7_Electronics"
-// 11 = Gen7 v1.1, v1.2 = 11
-// 12 = Gen7 v1.3
-// 13 = Gen7 v1.4
-// 3  = MEGA/RAMPS up to 1.2 = 3
-// 33 = RAMPS 1.3 / 1.4 (Power outputs: Extruder, Bed, Fan)
-// 34 = RAMPS 1.3 / 1.4 (Power outputs: Extruder0, Extruder1, Bed)
-// 4  = Duemilanove w/ ATMega328P pin assignment
-// 5  = Gen6
-// 51 = Gen6 deluxe
-// 6  = Sanguinololu < 1.2
-// 62 = Sanguinololu 1.2 and above
-// 63 = Melzi
-// 64 = STB V1.1
-// 7  = Ultimaker
-// 71 = Ultimaker (Older electronics. Pre 1.5.4. This is rare)
-// 8  = Teensylu
-// 80 = Rumba
-// 81 = Printrboard (AT90USB1286)
-// 82 = Brainwave (AT90USB646)
-// 9  = Gen3+
-// 70 = Megatronics
-// 90 = Alpha OMCA board
-// 91 = Final OMCA board
-// 301 = Rambo
-
 #ifndef MOTHERBOARD
-#define MOTHERBOARD 35
+    #define MOTHERBOARD 35
 #endif
 
 //// The following define selects which power supply you have. Please choose the one that matches your setup
 // 1 = ATX
 // 2 = X-Box 360 203Watts (the blue wire connected to PS_ON and the red wire to VCC)
-
 #define POWER_SUPPLY 1
 
 //===========================================================================
 //=============================Thermal Settings  ============================
 //===========================================================================
-//
-//--NORMAL IS 4.7kohm PULLUP!-- 1kohm pullup can be used on hotend sensor, using correct resistor and table
-//
-//// Temperature sensor settings:
-// -2 is thermocouple with MAX6675 (only for sensor 0)
-// -1 is thermocouple with AD595
-// 0 is not used
-// 1 is 100k thermistor - best choice for EPCOS 100k (4.7k pullup)
-// 2 is 200k thermistor - ATC Semitec 204GT-2 (4.7k pullup)
-// 3 is mendel-parts thermistor (4.7k pullup)
-// 4 is 10k thermistor !! do not use it for a hotend. It gives bad resolution at high temp. !!
-// 5 is 100K thermistor - ATC Semitec 104GT-2 (Used in ParCan) (4.7k pullup)
-// 6 is 100k EPCOS - Not as accurate as table 1 (created using a fluke thermocouple) (4.7k pullup)
-// 7 is 100k Honeywell thermistor 135-104LAG-J01 (4.7k pullup)
-// 8 is 100k 0603 SMD Vishay NTCS0603E3104FXT (4.7k pullup)
-// 9 is 100k GE Sensing AL03006-58.2K-97-G1 (4.7k pullup)
-// 10 is 100k RS thermistor 198-961 (4.7k pullup)
-//
-//    1k ohm pullup tables - This is not normal, you would have to have changed out your 4.7k for 1k 
-//                          (but gives greater accuracy and more stable PID)
-// 51 is 100k thermistor - EPCOS (1k pullup)
-// 52 is 200k thermistor - ATC Semitec 204GT-2 (1k pullup)
-// 55 is 100k thermistor - ATC Semitec 104GT-2 (Used in ParCan) (1k pullup)
-
+// zim overrides in thermistortables.h
 #define TEMP_SENSOR_0 77
 #define TEMP_SENSOR_1 77
 #define TEMP_SENSOR_2 77
@@ -120,36 +67,26 @@
 #define PIDTEMP
 #define BANG_MAX 256 // limits current to nozzle while in bang-bang mode; 256=full current
 #define PID_MAX 256 // limits current to nozzle while PID is active (see PID_FUNCTIONAL_RANGE below); 256=full current
+
 #ifdef PIDTEMP
-  //#define PID_DEBUG // Sends debug data to the serial port. 
-  //#define PID_OPENLOOP 1 // Puts PID in open loop. M104/M140 sets the output power from 0 to PID_MAX
-  #define PID_FUNCTIONAL_RANGE 10 // If the temperature difference between the target temperature and the actual temperature
-                                  // is more then PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
-  #define PID_INTEGRAL_DRIVE_MAX 255  //limit for the integral term
-  #define K1 0.95 //smoothing factor withing the PID
-  #define PID_dT ((16.0 * 8.0)/(F_CPU / 64.0 / 256.0)) //sampling period of the temperature routine
+    //#define PID_DEBUG // Sends debug data to the serial port.
+    //#define PID_OPENLOOP 1 // Puts PID in open loop. M104/M140 sets the output power from 0 to PID_MAX
+    #define PID_FUNCTIONAL_RANGE 10 // If the temperature difference between the target temperature and the actual temperature
+                                    // is more then PID_FUNCTIONAL_RANGE then the PID will be shut off and the heater will be set to min/max.
+    #define PID_INTEGRAL_DRIVE_MAX 255  //limit for the integral term
+    #define K1 0.95 //smoothing factor withing the PID
+    #define PID_dT ((16.0 * 8.0)/(F_CPU / 64.0 / 256.0)) //sampling period of the temperature routine
 
-
-// If you are using a preconfigured hotend then you can use one of the value sets by uncommenting it
-// Zeepro Zim
+    // If you are using a preconfigured hotend then you can use one of the value sets by uncommenting it
+    // Zeepro Zim
     #define  DEFAULT_Kp 40
     #define  DEFAULT_Ki 4
     #define  DEFAULT_Kd 95
 
-//Second Extruder parameters
+    //Second Extruder parameters
     #define  DEFAULT_Kp2 40
     #define  DEFAULT_Ki2 4
     #define  DEFAULT_Kd2 95
-
-// Makergear
-//    #define  DEFAULT_Kp 7.0
-//    #define  DEFAULT_Ki 0.1  
-//    #define  DEFAULT_Kd 12  
-
-// Mendel Parts V9 on 12V    
-//    #define  DEFAULT_Kp 63.0
-//    #define  DEFAULT_Ki 2.25
-//    #define  DEFAULT_Kd 440
 #endif // PIDTEMP
 
 // Bed Temperature Control
@@ -173,19 +110,19 @@
 #define MAX_BED_POWER 256 // limits duty cycle to bed; 256=full current
 
 #ifdef PIDTEMPBED
-//120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
-//from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, argressive factor of .15 (vs .1, 1, 10)
+    //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
+    //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, argressive factor of .15 (vs .1, 1, 10)
     #define  DEFAULT_bedKp 10.00
     #define  DEFAULT_bedKi .023
     #define  DEFAULT_bedKd 305.4
 
-//120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
-//from pidautotune
-//    #define  DEFAULT_bedKp 97.1
-//    #define  DEFAULT_bedKi 1.41
-//    #define  DEFAULT_bedKd 1675.16
+    //120v 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
+    //from pidautotune
+    //    #define  DEFAULT_bedKp 97.1
+    //    #define  DEFAULT_bedKi 1.41
+    //    #define  DEFAULT_bedKd 1675.16
 
-// FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
+    // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
 
 
@@ -203,7 +140,6 @@
 //=============================Mechanical Settings===========================
 //===========================================================================
 
-
 // Uncomment the following line to enable CoreXY kinematics
 // #define COREXY
 
@@ -211,78 +147,49 @@
 #define ENDSTOPPULLUPS // Comment this out (using // at the start of the line) to disable the endstop pullup resistors
 
 #ifndef ENDSTOPPULLUPS
-  // fine Enstop settings: Individual Pullups. will be ignord if ENDSTOPPULLUPS is defined
-  #define ENDSTOPPULLUP_XMAX
-  #define ENDSTOPPULLUP_YMAX
-  #define ENDSTOPPULLUP_ZMAX
-  #define ENDSTOPPULLUP_XMIN
-  #define ENDSTOPPULLUP_YMIN
-  #define ENDSTOPPULLUP_ZMIN
+    // fine Enstop settings: Individual Pullups. will be ignord if ENDSTOPPULLUPS is defined
+    #define ENDSTOPPULLUP_XMAX
+    #define ENDSTOPPULLUP_YMAX
+    #define ENDSTOPPULLUP_ZMAX
+    #define ENDSTOPPULLUP_XMIN
+    #define ENDSTOPPULLUP_YMIN
+    #define ENDSTOPPULLUP_ZMIN
     /// ici j'ai ajout�/////
-  #define ENDSTOPPULLUP_PRIVATE_ENDSTOPS1
-  #define ENDSTOPPULLUP_PRIVATE_ENDSTOPS2
-  #define ENDSTOPPULLUP_Endstop_voyage
-  #define ENDSTOPPULLUP_Endstop_Z_Movement
-  #define ENDSTOPPULLUP_Endstop_Carre
-  ///// c est bon ///
+    #define ENDSTOPPULLUP_PRIVATE_ENDSTOPS1
+    #define ENDSTOPPULLUP_PRIVATE_ENDSTOPS2
+    #define ENDSTOPPULLUP_Endstop_voyage
+    #define ENDSTOPPULLUP_Endstop_Z_Movement
+    #define ENDSTOPPULLUP_Endstop_Carre
+    ///// c est bon ///
 #endif
 
 #ifdef ENDSTOPPULLUPS
-  #define ENDSTOPPULLUP_XMAX
-  #define ENDSTOPPULLUP_YMAX
-  #define ENDSTOPPULLUP_ZMAX
-  #define ENDSTOPPULLUP_XMIN
-  #define ENDSTOPPULLUP_YMIN
-  #define ENDSTOPPULLUP_ZMIN
+    #define ENDSTOPPULLUP_XMAX
+    #define ENDSTOPPULLUP_YMAX
+    #define ENDSTOPPULLUP_ZMAX
+    #define ENDSTOPPULLUP_XMIN
+    #define ENDSTOPPULLUP_YMIN
+    #define ENDSTOPPULLUP_ZMIN
       /// ici j'ai ajout�/////
- #define ENDSTOPPULLUP_PRIVATE_ENDSTOPS1
- #define ENDSTOPPULLUP_PRIVATE_ENDSTOPS2
- #define ENDSTOPPULLUP_Endstop_voyage
-  #define ENDSTOPPULLUP_Endstop_Z_Movement
-  #define ENDSTOPPULLUP_Endstop_Carre
-  ///// c est bon ///
+    #define ENDSTOPPULLUP_PRIVATE_ENDSTOPS1
+    #define ENDSTOPPULLUP_PRIVATE_ENDSTOPS2
+    #define ENDSTOPPULLUP_Endstop_voyage
+    #define ENDSTOPPULLUP_Endstop_Z_Movement
+    #define ENDSTOPPULLUP_Endstop_Carre
+    ///// c est bon ///
 #endif
 
 
 // POSITION TRANSPORT //////
 #define Fonction_transport_Define
-
-
-// Fonction Power button
 #define Power_Button
-
-
-// Fonction LOW POWER STANDBY
 #define LOW_POWER_STANDBY
-
-
-
-
 // Fonction Pour le mouvement de palpation de Z
 #define Fonction_Z_Movement_Define
-
-
-
-
 // Fonction Pour le mouvement du carr�
 #define Fonction_Carre_Define
-
-
-
-
-
-
 // Fonction arrondir en plus
-
 #define Calcul_arrondir_en_plus
-
-
-
-
-
-
-
-
 
 // Fonction Certification Julia
 // PNI 20140916 change define name from CERTIFICATION to TEST
@@ -308,7 +215,6 @@
 #define DISTANCE_EXTRUDE_RETRACT	4
 
 //Unloading
-
 #define UNLOADING_PART1_LENGTH			100		// mm
 #define UNLOADING_PART1_FEEDRATE		2000	// mm/mn
 #define UNLOADING_PART2_LENGTH_LEFT		1330	// mm
@@ -380,29 +286,17 @@
 #define NUM_AXIS 4 // The axis order in all axis related arrays is X, Y, Z, E
 #define NUM_STOP_PARAMETERS 6 // The axis order in all axis related arrays is X, Y, Z, T0, T1, Bed
 #define HOMING_FEEDRATE {1500, 1500, 500, 0}  // set the homing speeds (mm/min)
-	
 
-
-	/*
-	
-	
-	Pour les axis steps, avec tes poulies 15 dents, le calcul est simple � faire
-
+/*
+Pour les axis steps, avec tes poulies 15 dents, le calcul est simple � faire
 15 dents x 2mm (poulies GT2) = 30 mm de circonf�rence
-
 1 tour = 32 micro pas x 200 pas (1,8� d'angle) = 6400 micro pas
-
-axis steps = nombre de micro pas pour faire un mm = 6400/30 = 213	
-	
-	
-	*/
-
+axis steps = nombre de micro pas pour faire un mm = 6400/30 = 213
+*/
 
 // default settings 
-
-
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {201,201,3200,316} // extruder (last parameter) was 290  
-#define DEFAULT_MAX_FEEDRATE          {1000, 1000, 100, 1000}    // (mm/sec)    
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {201,201,3200,316}        // extruder (last parameter) was 290
+#define DEFAULT_MAX_FEEDRATE          {1000, 1000, 100, 1000}   // (mm/sec)
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}     // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          1000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves 
@@ -415,12 +309,11 @@ axis steps = nombre de micro pas pour faire un mm = 6400/30 = 213
 /////// GRISE///////////////
  #define EXTRUDER_OFFSET_X {0.0, -15} // (in mm) for each extruder, offset of the hotend on the X axis
  #define EXTRUDER_OFFSET_Y {0.0, -1.7}  // (in mm) for each extruder, offset of the hotend on the Y axis
-	 
-	 
-	 /*
- #define EXTRUDER_OFFSET_X {0.0, -15} // (in mm) for each extruder, offset of the hotend on the X axis
- #define EXTRUDER_OFFSET_Y {0.0, -1.7}  // (in mm) for each extruder, offset of the hotend on the Y axis
-	 */
+
+ /*
+#define EXTRUDER_OFFSET_X {0.0, -15} // (in mm) for each extruder, offset of the hotend on the X axis
+#define EXTRUDER_OFFSET_Y {0.0, -1.7}  // (in mm) for each extruder, offset of the hotend on the Y axis
+ */
 
 // The speed change that does not require acceleration (i.e. the software might assume it can be done instanteneously)
 #define DEFAULT_XYJERK                20.0    // (mm/sec)
@@ -460,8 +353,8 @@ axis steps = nombre de micro pas pour faire un mm = 6400/30 = 213
 
 //automatic expansion
 #if defined(ULTIMAKERCONTROLLER) || defined(REPRAP_DISCOUNT_SMART_CONTROLLER) || defined(G3D_PANEL)
- #define ULTIPANEL
- #define NEWPANEL
+    #define ULTIPANEL
+    #define NEWPANEL
 #endif 
 
 // Preheat Constants
